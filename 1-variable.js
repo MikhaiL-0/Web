0bdb27606a5f1606c8562f66c29d876fd765a5f4
Mikhail Fedosov <mishafedosov1995@gmail.com>
2023-08-30 20:44:11 +0300

    learn

---
+++ 1-variable.js
@@ -0,0 +1,17 @@
+/**
+ * ПЕРЕМЕННЫЕ
+ */
+
+// Объявление
+let name;
+
+// Инициализация
+name = 'Миша';
+console.log(name);
+// Объявление + инициализация
+let surname = 'Федосов';
+console.log(surname);
+// Типы переменных в JavaScript: let
+
+// Типы переменных в JavaScript: const
+const adultAge = 18;
